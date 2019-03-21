9471f1a6ad5fe659d857749ce92d083e54b15349
DAVID SILVA SOUZA <18175740@cfp127aluno.local>
2019-03-21 17:01:04 -0300

    Cadastro Activity funcionando com img na view

--- CatalogodeFilmes/app/src/main/java/br/senai/sp/catalogodefilmes/CadastroActivity.java
+++ CatalogodeFilmes/app/src/main/java/br/senai/sp/catalogodefilmes/CadastroActivity.java
@@ -2,6 +2,10 @@ package br.senai.sp.catalogodefilmes;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.renderscript.ScriptGroup;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,20 +14,53 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 import br.senai.sp.dao.FilmeDAO;
 import br.senai.sp.modelo.Filme;
 
 public class CadastroActivity extends AppCompatActivity {
-    CadastroFilmeHelper helper;
-
+    private  CadastroFilmeHelper helper;
+    private  Button btn_camera;
+    private  Button btn_galeria;
+    private ImageView imgFoto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
 
         helper = new CadastroFilmeHelper(this);
+        // associando a variavel para a view
+        btn_camera = findViewById(R.id.btn_foto_camera);
+
+        btn_galeria = findViewById(R.id.btn_foto_galeria);
+
+        imgFoto = findViewById(R.id.image_view_filme);
+
+
+        //ouvintes para os botões
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CadastroActivity.this, "chamando camera", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btn_galeria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CadastroActivity.this, "chamando galeria", Toast.LENGTH_LONG).show();
+                Intent intentGaleria = new Intent(Intent.ACTION_GET_CONTENT);
+                intentGaleria.setType("image/*");
+                startActivityForResult(intentGaleria, 1000);
+            }
+        });
+
 
         Intent intent = getIntent();
 
@@ -34,6 +71,24 @@ public class CadastroActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Toast.makeText(this, "A foto é essa",Toast.LENGTH_LONG).show();
+
+        try {
+            InputStream inputStream = getContentResolver()
+                    .openInputStream(data.getData());
+
+            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+            imgFoto.setImageBitmap(bitmap);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -73,7 +128,7 @@ public class CadastroActivity extends AppCompatActivity {
                 }else{
 
                     AlertDialog.Builder caixaDialogo = new AlertDialog.Builder(this);
-                    caixaDialogo.setTitle("Exluindo um filme");
+                    caixaDialogo.setTitle("Exluindo " + filme.getTitulo());
                     caixaDialogo.setMessage("você tem certeza que quer excluir "+ filme.getTitulo() +"?");
                     caixaDialogo.setPositiveButton("SIM", new DialogInterface.OnClickListener() {
                         @Override
@@ -81,7 +136,6 @@ public class CadastroActivity extends AppCompatActivity {
                             dao.excluir(filme);
                             Toast.makeText(CadastroActivity.this, filme.getTitulo() + " foi Excluído", Toast.LENGTH_LONG).show();
                             dao.close();
-
                         }
                     });
                     caixaDialogo.setNegativeButton("NÃO", null);
